b9019dde4fd14467b75e5fd99f6539735cae1971
pasocon-renai <aili.nierengarten@proton.me>
2026-07-24 22:51:52 +0000

    Add files via upload

---
+++ src/model/stablediffusion/load.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+
+use burn::{
+    module::Param,
+    tensor::backend::Backend,
+};
+
+use super::*;
+use crate::model::{
+    autoencoder::load::load_autoencoder, clip::load::load_clip, load::*, unet::load::load_unet,
+};
+
+pub fn load_stable_diffusion<B: Backend>(
+    path: &str,
+    device: &B::Device,
+) -> Result<StableDiffusion<B>, Box<dyn Error>> {
+    let n_steps = load_usize::<B>("n_steps", path, device)?;
+    let alpha_cumulative_products = Param::from_tensor(load_tensor::<B, 1>("alphas_cumprod", path, device)?);
+    let autoencoder = load_autoencoder(&format!("{}/{}", path, "autoencoder"), device)?;
+    let diffusion = load_unet(&format!("{}/{}", path, "unet"), device)?;
+    let clip = load_clip(&format!("{}/{}", path, "clip"), device)?;
+
+    Ok(StableDiffusion {
+        n_steps,
+        alpha_cumulative_products,
+        autoencoder,
+        diffusion,
+        clip,
+    })
+}
